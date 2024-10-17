b8e404ae51778b87389be25fc50c8cb2febf7a9e
Popov Nikita <popovnikita01@gmail.com>
2024-10-17 12:12:17 +0200

    fix script

--- cli.sh
+++ cli.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-command="$@"
-
-docker compose -f compose.yaml exec -e CLI_PROG_NAME="marzban cli" marzban marzban-cli "$command"
+docker compose -f compose.yaml exec -e CLI_PROG_NAME="marzban cli" marzban marzban-cli "$@"
